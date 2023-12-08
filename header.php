2a1446f3379e6a272d2f634056f92dd9ffdc05cf
Joepriestley <joeamonoo1114@gmail.com>
2023-12-08 13:41:53 +0100

    update geoportal to pfe_work

--- header.php
+++ header.php
@@ -14,11 +14,11 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
     <meta name="description" content="Geoportail, SIG-WEB, Développement de SIG Web, Webmapping">
     <title>Geoportail-PNSM</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/geoportal-pnsm/css/styles.css"> <!-- Your custom CSS file for further customization -->
-    <link rel="stylesheet" href="/geoportal-pnsm/lightbox/lightbox.css">
-    <link rel="stylesheet" href="/geoportal-pnsm/css/style.css">
-    <link rel="stylesheet" href="/geoportal-pnsm/css/nav.css">
-    <link rel="stylesheet" href="/geoportal-pnsm/css/apropos.css">
+    <link rel="stylesheet" href="/pfe_work/css/styles.css"> <!-- Your custom CSS file for further customization -->
+    <link rel="stylesheet" href="/pfe_work/lightbox/lightbox.css">
+    <link rel="stylesheet" href="/pfe_work/css/style.css">
+    <link rel="stylesheet" href="/pfe_work/css/nav.css">
+    <link rel="stylesheet" href="/pfe_work/css/apropos.css">
     <style>
         header {
             position: fixed;
@@ -35,8 +35,8 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light" style="background-image: linear-gradient( 109.6deg,  rgba(61,131,97,1) 11.2%, rgba(28,103,88,1) 91.1% );">
-            <a class="navbar-brand" href="/geoportal-pnsm/index.php" style="color: white;">
-                <img src="/geoportal-pnsm/img/pnsm.png" alt="Geoportail-PNSM logo" width="40" height="40">
+            <a class="navbar-brand" href="/pfe_work/index.php" style="color: white;">
+                <img src="/pfe_work/img/pnsm.png" alt="Geoportail-PNSM logo" width="40" height="40">
                 Geoportail-PNSM
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
@@ -45,13 +45,13 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
             <div class="collapse navbar-collapse" id="navbarMenu">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a href="/geoportal-pnsm/index.php" class="nav-link" style="color: white;">Accueil</a>
+                        <a href="/pfe_work/index.php" class="nav-link" style="color: white;">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/geoportal-pnsm/apropos.php" class="nav-link" style="color: white;">A propos</a>
+                        <a href="/pfe_work/apropos.php" class="nav-link" style="color: white;">A propos</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/geoportal-pnsm/galerie.php" class="nav-link" style="color: white;">Galerie</a>
+                        <a href="/pfe_work/galerie.php" class="nav-link" style="color: white;">Galerie</a>
                     </li>
                     <?php if (isset($_SESSION['nom']) && $_SESSION['nom'] != '') { ?>
 
@@ -60,11 +60,11 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
                                 Gestion
                             </a>
                             <div class="dropdown-menu" aria-labelledby="gestionDropdown">
-                                <a class="dropdown-item" href="/geoportal-pnsm/amenagement.php">Amenagement</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/refection_pistes.php">Pistes</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/ref_point_eau.php">Point Eau</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/ref_amenagementtouristique.php">Amenagement Touristiques</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/refection_cloture.php">Cloture</a>
+                                <a class="dropdown-item" href="/pfe_work/amenagement.php">Amenagement</a>
+                                <a class="dropdown-item" href="/pfe_work/refection_pistes.php">Pistes</a>
+                                <a class="dropdown-item" href="/pfe_work/ref_point_eau.php">Point Eau</a>
+                                <a class="dropdown-item" href="/pfe_work/ref_amenagementtouristique.php">Amenagement Touristiques</a>
+                                <a class="dropdown-item" href="/pfe_work/refection_cloture.php">Cloture</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
@@ -72,18 +72,18 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
                                 Ecodeveloppement
                             </a>
                             <div class="dropdown-menu" aria-labelledby="ecodeveloppementDropdown">
-                                <a class="dropdown-item" href="/geoportal-pnsm/touriste.php">Touristes</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/circuittourist.php">Circuit</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/passage.php">Circuit Passage</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/visitesite.php">Site Visite</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/apiculture.php">Alpiculture</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/percheartisanal.php">Perche Artisanal</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/agriculture.php">Agriculture</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/elevage.php">Elevage</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/autreactivite.php">Autres Activites</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/infrastruturetouristiq.php">Infrastructure Touristiques</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/utilise.php">Utilisation</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/association.php">Associations</a>
+                                <a class="dropdown-item" href="/pfe_work/touriste.php">Touristes</a>
+                                <a class="dropdown-item" href="/pfe_work/circuittourist.php">Circuit</a>
+                                <a class="dropdown-item" href="/pfe_work/passage.php">Circuit Passage</a>
+                                <a class="dropdown-item" href="/pfe_work/visitesite.php">Site Visite</a>
+                                <a class="dropdown-item" href="/pfe_work/apiculture.php">Alpiculture</a>
+                                <a class="dropdown-item" href="/pfe_work/percheartisanal.php">Perche Artisanal</a>
+                                <a class="dropdown-item" href="/pfe_work/agriculture.php">Agriculture</a>
+                                <a class="dropdown-item" href="/pfe_work/elevage.php">Elevage</a>
+                                <a class="dropdown-item" href="/pfe_work/autreactivite.php">Autres Activites</a>
+                                <a class="dropdown-item" href="/pfe_work/infrastruturetouristiq.php">Infrastructure Touristiques</a>
+                                <a class="dropdown-item" href="/pfe_work/utilise.php">Utilisation</a>
+                                <a class="dropdown-item" href="/pfe_work/association.php">Associations</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
@@ -91,11 +91,11 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
                                 Ressources Naturelles
                             </a>
                             <div class="dropdown-menu" aria-labelledby="ressourcesDropdown">
-                                <a class="dropdown-item" href="/geoportal-pnsm/esp_vegetal.php">Flore</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/sol.php">Sol</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/esp_animal.php">Faune</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/recensement.php">Recensement</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/suivi_faune.php">Suivi Faune</a>
+                                <a class="dropdown-item" href="/pfe_work/esp_vegetal.php">Flore</a>
+                                <a class="dropdown-item" href="/pfe_work/sol.php">Sol</a>
+                                <a class="dropdown-item" href="/pfe_work/esp_animal.php">Faune</a>
+                                <a class="dropdown-item" href="/pfe_work/recensement.php">Recensement</a>
+                                <a class="dropdown-item" href="/pfe_work/suivi_faune.php">Suivi Faune</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
@@ -103,18 +103,18 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
                                 Projets
                             </a>
                             <div class="dropdown-menu" aria-labelledby="projetsDropdown">
-                                <a class="dropdown-item" href="/geoportal-pnsm/projet.php"">Projet</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/partenaire.php">Partenaire</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/association.php">Associations</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/executeur.PHP">Executeur</a>
-                                <a class="dropdown-item" href="/geoportal-pnsm/action.php">Actions</a>
+                                <a class="dropdown-item" href="/pfe_work/projet.php"">Projet</a>
+                                <a class="dropdown-item" href="/pfe_work/partenaire.php">Partenaire</a>
+                                <a class="dropdown-item" href="/pfe_work/association.php">Associations</a>
+                                <a class="dropdown-item" href="/pfe_work/executeur.PHP">Executeur</a>
+                                <a class="dropdown-item" href="/pfe_work/action.php">Actions</a>
                             </div>
                         </li>
 
                     <?php } ?>
 
                     <li class="nav-item">
-                        <a href="/geoportal-pnsm/webmap/map.php" class="nav-link" style="color: white;">Cartographie</a>
+                        <a href="/pfe_work/webmap/map.php" class="nav-link" style="color: white;">Cartographie</a>
                     </li>
                     <li>
 
@@ -124,17 +124,17 @@ if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') {
                     ?>
 
                             <li class="nav-item">
-                                <a href="/geoportal-pnsm/sign-up.php"  class="nav-link" style="color: white;">Inscrivez-vous</a>
+                                <a href="/pfe_work/sign-up.php"  class="nav-link" style="color: white;">Inscrivez-vous</a>
                             </li>
                     <?php }
                     } ?>
 
                     <li class="nav-item">
                         <?php if (!isset($_SESSION['nom'])  || $_SESSION['nom'] === '') { ?>
-                            <a href="/geoportal-pnsm/login.php" class="nav-link" style="color: white;">Authentifiez-vous</a>
+                            <a href="/pfe_work/login.php" class="nav-link" style="color: white;">Authentifiez-vous</a>
 
                         <?php } else { ?>
-                            <a href="/geoportal-pnsm/logout.php" class="nav-link" style="color: white;">Logout</a>
+                            <a href="/pfe_work/logout.php" class="nav-link" style="color: white;">Logout</a>
 
                         <?php } ?>
 
